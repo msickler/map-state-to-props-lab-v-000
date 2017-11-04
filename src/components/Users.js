489a76d7e69665bf396600021d75a88c74e6c2c7
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-04 13:59:13 +0000

    automatically backed up by learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,9 +4,8 @@ import { connect } from 'react-redux';
 export class Users extends Component {
 
   render() {
-    const users = this.props.users.map((user, index) => {
-      <li key={index}>{user.userName}</li>
-    })
+    let users = this.props.users.map((user, index) => <li key={index}>{user.userName}</li>);
+
     return (
       <div>
         <ul>
@@ -18,7 +17,10 @@ export class Users extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return {users: state.users, primaryUser: state.users[0]}
-}
+  return {
+    users: state.users,
+    primaryUser: state.users[0]
+  };
+};
 
 export const ConnectedUsers = connect(mapStateToProps)(Users)
